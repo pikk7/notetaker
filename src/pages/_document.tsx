9cd6b4c326229c680e6eac4eb3dcba31fb0ae3d1
pikk7 <durza13@gmail.com>
2023-06-12 14:57:15 +0200

    UI update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Main, NextScript, Head } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="winter">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
